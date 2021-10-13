c4414f19605a559632ef4f18d29981e72fa52124
Anthony Zamora <84356810+xBOBOYx@users.noreply.github.com>
2021-10-13 16:42:45 -0700

    atlas

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 // routes
