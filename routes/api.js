fd6cd47f63938e23b5c57462c3115aeae33f8d60
Anthony Zamora <84356810+xBOBOYx@users.noreply.github.com>
2021-10-13 15:59:38 -0700

    fixed server and routes

--- routes/api.js
+++ routes/api.js
@@ -1,58 +1,81 @@
-const router = require('express').Router();
-const mongoose = require('mongoose');
-const Workout = require('../models/workout.js');
-const express = require('express');
+const router = require("express").Router();
+const Workout = require("../models/workout");
+const path = require("path");
 
-
-router.post("/api/workouts", ({ body }, res) => {
-  Workout.create({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+router.get('/api/workouts', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration',
+                },
+            },
+        },
+    ])
+        .then((dbWorkouts) => {
+            res.json(dbWorkouts);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
 });
 
-router.put("/api/workouts/:id", ({ body, params }, res) => {
-    Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
-      .then(dbWorkout => {
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration',
+                },
+            },
+        },
+    ])
+        .then((dbWorkouts) => {
+            res.json(dbWorkouts);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+
+});
+
+
+router.post("/api/workouts", ({
+    body
+}, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findOneAndUpdate({
+        _id: req.params.id
+    }, {
+        $push: {
+            exercises: req.body
+        }
+    }, {
+        new: true
+    })
+    .then(dbWorkout => {
         res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
-
-router.get("/api/workouts", ({ body }, res) => {
-  Workout.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
+    }).catch(err => {
+        res.json(err);
     });
 });
 
-
-router.get("/api/workouts/range", ({ body }, res) => {
-  Workout.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+router.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-router.get("/api/workouts/:id", ({ params }, res) => {
-  Workout.findById(params.id)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+router.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-const express= require('express');
-const morgan= require('morgan');
-const mongoose= require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
 
-const PORT= process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
-const app= express();
+const app = express();
+app.use(logger("dev"));
 
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
-  useCreateIndex: true,
 });
 
 // routes
-app.use(require("./routes/html.js"));
 app.use(require("./routes/api.js"));
 
+
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+  console.log(`App running on port ${PORT}!`);
+});
